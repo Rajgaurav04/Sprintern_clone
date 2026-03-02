670e951a8f43412c7f337637ba880aa1d665bd81
Raj Gaurav <rajdeo8052@gmail.com>
2026-03-02 09:36:43 +0530

    Create Button of landingPage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -13,11 +13,16 @@ const LandingPage = () => {
          <div className="w-1/2 flex flex-col justify-center items-start px-20 py-6">
          
          
-         <p className="text-sm text-gray-300 px-4 font-semibold py-[6pxg] bg-linear-to-r rounded-full mb-3 border  border-purple-500 ">Limited seats available!</p>
+         <p className="text-xs text-gray-300 px-4 font-semibold py-[6px] bg-linear-to-br from-emerald-500/10 to-purple-500/30 rounded-full mb-3  border  border-purple-500 ">LIMITED SEATS AVAILABLE!</p>
             <h1 className="text-7xl font-bold text-white mb-6 ">Master Engineering Internships in <span className="whitespace-nowrap bg-linear-to-r from-sky-500 via-emerald-400 to-pink-400 text-transparent bg-clip-text">Just 28 Days</span></h1>
             <p className="text-lg text-gray-300 mb-8">Intensive courses designed to bridge the gap between academia and industry through project mastery and real startup exposure.</p>
+
+            <div className="flex flex-row space-x-4">
+              <button className="px-6 py-3 bg-linear-to-r from-sky-500 to-emerald-700 rounded-full font-semibold text-white text-md hover:from-indigo-600 to-purple-800 cursor-pointer">Start Journey</button>
+              <button  className="px-6 py-3 rounded-full font-semibold text-white border-2 border-emerald-400 text-md hover:bg-linear-to-r from-emerald-500 to-blue-700 cursor-pointer">Explore Courses </button>
+            </div>
           </div>
-          <div className="p-10 w-1/2 flex justify-center items-center ">
+          <div className="p-8 w-1/2 flex justify-center items-center ">
           <DotLottieReact
       src="https://lottie.host/6331af21-d8ee-4733-b36c-9712bb3709f4/bjndqv6lEA.lottie"
       loop
